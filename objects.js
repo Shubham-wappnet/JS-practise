b0655be98f5198c6f2c5ee1dab9e8a6e7350c72d
Shubham-wappnet <shubham.wappnet@gmail.com>
2024-02-06 18:10:04 +0530

    js-objects with changes

--- objects.js
+++ objects.js
@@ -1,99 +1,99 @@
-// const sample= { cylinders: 4, size: 2.2 };
-// const myHonda = {
-//     color: "red",
-//     wheels: 4,
-//     engine:sample
-//   };
+const sample= { cylinders: 4, size: 2.2 };
+const myHonda = {
+    color: "red",
+    wheels: 4,
+    engine:sample
+  };
 
-// console.log(myHonda.engine); 
-// delete myHonda.color;       //delete property
-// console.log(myHonda); 
-// let x=myHonda.engine;       //delete object of object
-// console.log(x)
-// delete x.cylinders;
-// console.log(myHonda); 
-
-
-
-//////
-// function person(name,age,city){
-//    this.name=name;
-//    this.age=age;
-//    this.city=city;
-//    console.log(`${this.name}`+""+`${this.age}`+""+`${this.city}`)
-// }
-
-// const p1=new person("Rohan",22,"ahe");
-// const p2=new person("Mihir",21,"ank");
-
-// /////
-// const person={
-//     name:"Shubham Lad",
-//     age:21,
-//     company:"Wappnet",
-//     showInfo:function(){
-//         if(typeof(person.name)==typeof(person.age)){
-//             console.log(`${person.name} ${person.age}`);
-//         }
-//         else {
-//              console.log(`${person.name}` + "is working at" +`${person.company}`);
-//         }
-//     }
-
-// }
-// console.log(person.showInfo());
+console.log(myHonda.engine); 
+delete myHonda.color;       //delete property
+console.log(myHonda); 
+let x=myHonda.engine;       //delete object of object
+console.log(x)
+delete x.cylinders;
+console.log(myHonda); 
 
 
-// /////
-// const Animal = {
-//     type: "Invertebrates", 
-//     displayType() {
-//       console.log(this.type);
-//     },
-//   };
-  
-//   const animal1 = Object.create(Animal);
-//   animal1.displayType(); 
 
-//   const animal2 = Object.create(Animal);
-//   animal2.type = "Fishes";
-//   animal2.displayType(); 
-
-//////
-// const person = {
-//     name: "John",
-//     age: 30,
-//     city: "New York"
-//   };
-//   person.age=person.age.toString();
-//   console.log(person)
-  
-//   let txt = "";
-//   for (let x in person) {
-//     txt += person[x] + " " ;
-//   };
-//   console.log(txt);
-//  const arr1=Object.values(txt);
-//   console.log(arr1)
-
-//   let str=JSON.stringify(person);
-//   console.log(str)
-  
 ////
-// function f1(){
-//     let n=0;
-//     return{
-//         next:function(){
-//             n+=10;
-//             return{
-//                 value:n,done:false
-//             }
-//         }
-//     }
-// }
-// const n=f1();
-// console.log(n.next());
-// console.log(n.next());
+function person(name,age,city){
+   this.name=name;
+   this.age=age;
+   this.city=city;
+   console.log(`${this.name}`+""+`${this.age}`+""+`${this.city}`)
+}
+
+const p1=new person("Rohan",22,"ahe");
+const p2=new person("Mihir",21,"ank");
+
+/////
+const person={
+    name:"Shubham Lad",
+    age:21,
+    company:"Wappnet",
+    showInfo:function(){
+        if(typeof(person.name)==typeof(person.age)){
+            console.log(`${person.name} ${person.age}`);
+        }
+        else {
+             console.log(`${person.name}` + "is working at" +`${person.company}`);
+        }
+    }
+
+}
+console.log(person.showInfo());
+
+
+/////
+const Animal = {
+    type: "Invertebrates", 
+    displayType() {
+      console.log(this.type);
+    },
+  };
+  
+  const animal1 = Object.create(Animal);
+  animal1.displayType(); 
+
+  const animal2 = Object.create(Animal);
+  animal2.type = "Fishes";
+  animal2.displayType(); 
+
+////
+const person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+  };
+  person.age=person.age.toString();
+  console.log(person)
+  
+  let txt = "";
+  for (let x in person) {
+    txt += person[x] + " " ;
+  };
+  console.log(txt);
+ const arr1=Object.values(txt);
+  console.log(arr1)
+
+  let str=JSON.stringify(person);
+  console.log(str)
+  
+//
+function f1(){
+    let n=0;
+    return{
+        next:function(){
+            n+=10;
+            return{
+                value:n,done:false
+            }
+        }
+    }
+}
+const n=f1();
+console.log(n.next());
+console.log(n.next());
 
 ////
 const a = "a";
@@ -108,8 +108,8 @@ letters.add(c);
 const myletter=letters.values();
 console.log(myletter)
 
-let arr1=[1,2,3,4,5];
-let arr2=arr1.map((curele)=>{
+let arra=[1,2,3,4,5];
+let arr2=arra.map((curele)=>{
     return curele*2;
     
 }).filter((curele)=>{
